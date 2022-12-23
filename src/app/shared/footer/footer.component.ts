ff9bdf178fdb49032221fe7514704c08c8d80ed5
Jose Andres Saavedra Romero <josex.1995@hotmail.com>
2022-12-23 17:29:58 -0500

    Implementacion iconos de iconscout en el footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  links: Array<LinkModel> = [
+    {
+      link: 'https://www.linkedin.com/in/joséandressaavedraromero/',
+      icon: '<i class="uil uil-linkedin"></i>'
+    },
+    {
+      link: 'https://github.com/Jasr24',
+      icon: '<i class="uil uil-github"></i>'
+    },
+    {
+      link: 'mailto:josex.1995@hotmail.com',
+      icon: '<i class="uil uil-envelope"></i>'
+    }
+  ]
+
+}
+
+class LinkModel {
+  link!: string;
+  icon!: string;
 }
