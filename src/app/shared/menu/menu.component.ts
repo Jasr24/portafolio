d7bfe1c49c0872e45cb907e109627bff02394dc2
Jose Andres Saavedra Romero <josex.1995@hotmail.com>
2022-12-28 09:24:36 -0500

    Generalizando stylo de letra de texto para parrafo, agregando hoja de vida en PDF, arreglando la posicion del footer, animando el sidebar, modificando el componente del menú

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 
+  links: Array<LinkModel> = [
+    {
+      link: 'https://www.linkedin.com/in/joséandressaavedraromero/',
+      icon: '<i class="uil uil-linkedin"></i>'
+    },
+    {
+      link: 'mailto:josex.1995@hotmail.com',
+      icon: '<i class="uil uil-envelope"></i>'
+    }
+  ]
+
+  curriculum = {
+    link: 'assets/HojaDeVida.pdf',
+    name: 'CV'
+  }
+  
+}
+
+class LinkModel {
+  link!: string;
+  icon!: string;
 }
