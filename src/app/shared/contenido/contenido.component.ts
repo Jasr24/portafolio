bf361ab71c6f492eb5b812981c67f3786194c830
Jose Andres Saavedra Romero <josex.1995@hotmail.com>
2023-01-04 19:04:36 -0500

    Arreglando alto del proyecto, en el sidebar y en el contenido; menu y footer modificando los botones de enlaces (hover)

--- src/app/shared/contenido/contenido.component.ts
+++ src/app/shared/contenido/contenido.component.ts
@@ -1,10 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contenido',
   templateUrl: './contenido.component.html',
   styleUrls: ['./contenido.component.css'],
-  //encapsulation: ViewEncapsulation.None
 })
 export class ContenidoComponent {
 
