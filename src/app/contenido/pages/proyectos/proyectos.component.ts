f562b3bb1b1779dd111560409564575ac0909bc2
Jose Andres Saavedra Romero <josex.1995@hotmail.com>
2023-01-05 14:58:43 -0500

    implementacion de animaciones con https://animate.style/

--- src/app/contenido/pages/proyectos/proyectos.component.ts
+++ src/app/contenido/pages/proyectos/proyectos.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from '../../services/github.service';
-import { map } from 'rxjs/operators';
 import { IGitMostrar } from '../../interfaces/general';
 
 @Component({
