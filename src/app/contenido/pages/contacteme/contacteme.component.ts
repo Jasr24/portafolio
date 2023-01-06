fb608c0f02765c91decfef7e6d349df8179c9d60
Jose Andres Saavedra Romero <josex.1995@hotmail.com>
2023-01-06 18:13:49 -0500

    Correcion palabra telfono por telefono

--- src/app/contenido/pages/contacteme/contacteme.component.ts
+++ src/app/contenido/pages/contacteme/contacteme.component.ts
@@ -35,7 +35,7 @@ export class ContactemeComponent {
     },
     {
       logo: '<i class="uil uil-phone"></i>',
-      nombre: 'Telfono movil',
+      nombre: 'Telefono movil',
       nombreLink: '+57 3219243790',
       url: 'https://api.whatsapp.com/send?phone=573219243790'
     }
